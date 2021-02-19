added normalization + fixed test.py

src/test.py
@@ -1,5 +1,6 @@
 from utils.configuration import Configuration
 from utils.model         import     SaveModel
+from utils.metrics       import       Metrics
 from utils.dataset       import       Dataset
 from utils.image         import         Image
 from pathlib             import          Path
@@ -26,19 +27,22 @@ if __name__ == "__main__":
    
     with torch.no_grad():
         loss = None
-        for i,(data,gold) in enumerate(testdataset,1):
+        nme  = None
+        for i,(data, gold, true, icds, size, bbox) in enumerate(testdataset,1):
             gold = gold.to(configuration.device)
             pred = torch.zeros(gold.shape).to(configuration.device)
             for j in range(68):
-                print(f"step:{i}/{len(testdataset)}, pred:{j}/68, test_loss:{loss}\r",end="")
+                print(f"step:{i}/{len(testdataset)}, pred:{j}/68, test_loss:{loss}, nme:{nme}\r",end="")
                 pred[:,j] = model(data,gold)[0][:,j]
-    
+            
+
             configuration.load()
             if configuration.show_images: Image.showall(data,pred,gold,configuration)
     
             loss = loss+(model.loss(pred,gold).item()-loss)/i if loss else model.loss(pred,gold).item()
-    
-    logging.info(f"epoch:{model.epoch}, valid_loss:{model.valid_loss}, test_loss:{loss}")
+            nme  = nme +(Metrics.normalized_mean_error(pred.to("cpu"),true,icds,size,bbox).item()-nme)/i if nme else Metrics.normalized_mean_error(pred.to("cpu"),true,icds,size,bbox).item()
+
+    logging.info(f"epoch:{model.epoch}, valid_loss:{model.valid_loss}, test_loss:{loss}, nme:{nme}")
     
     logging.info(f"{__file__.upper()} STARTING")
  

src/utils/FLTModel.py
@@ -25,6 +25,8 @@ class Model(torch.nn.Module):
         self.decoder   = torch.nn.TransformerDecoder(torch.nn.TransformerDecoderLayer(256,nhead=4,dim_feedforward=1024,activation="relu",dropout=.1),num_layers=2).to(configuration.device)
         self.peek      = self.generate_square_subsequent_mask(69).to(configuration.device)
 
+        self.normalize = torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])
+
         self.loss         = torch.nn.L1Loss(reduction="sum")
         self.optimizer    = torch.optim.Adam(self.parameters(),lr=0.0001)
 
@@ -38,10 +40,9 @@ class Model(torch.nn.Module):
             data = data.to(self.configuration.device)
             gold = gold.to(self.configuration.device)
 
-            res = self.resnet(data.transpose(1,3))
+            res = self.resnet(self.normalize(data.transpose(1,3)))
             res = res.view(res.size(0),res.size(1),res.size(2)*res.size(3))
 
-        
         srcs = self.src_upscale(res)
         tgts = torch.cat([self.sos.weight.unsqueeze(0).repeat(gold.size(0),1,1),self.tgt_upscale(gold)],1)
 