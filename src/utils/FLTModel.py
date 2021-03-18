0c3b24e16f138b8eaf853c0931d0282178a62927
Francesco Bertolotti <f14.bertolotti@gmail.com>
2021-03-18 11:47:43 +0100

    better model

--- src/utils/FLTModel.py
+++ src/utils/FLTModel.py
@@ -1,7 +1,11 @@
+
 from utils.image import Image
+import torch.nn.functional as fun
 
 import torchvision
+import einops
 import torch
+from utils.augmentations import Augment
 
 class Model(torch.nn.Module):
     def __init__(self, configuration):
@@ -9,30 +13,30 @@ class Model(torch.nn.Module):
 
         self.configuration = configuration
 
-        self.resnet = torchvision.models.resnet18(pretrained=True, progress=True).to(configuration.device)
-        self.resnet = torch.nn.Sequential(*list(self.resnet.children())[:8])
+        self.resnet = torchvision.models.resnet101(pretrained=True, progress=True).to(configuration.device)
+        self.resnet3 = torch.nn.Sequential(*list(self.resnet.children())[6:8])
+        self.resnet2 = torch.nn.Sequential(*list(self.resnet.children())[5:6])
+        self.resnet1 = torch.nn.Sequential(*list(self.resnet.children())[3:5])
+        self.resnet0 = torch.nn.Sequential(*list(self.resnet.children())[0:3])
 
-        self.eos          = torch.nn.Embedding(1,256).to(configuration.device)
-        self.sos          = torch.nn.Embedding(1,256).to(configuration.device)
-        self.src_position = torch.nn.Embedding(512,256).to(configuration.device)
-        self.tgt_position = torch.nn.Embedding(70,256).to(configuration.device)
+        self.reslin0 = torch.nn.Linear(256,2048).to(configuration.device)
+        self.reslin1 = torch.nn.Linear(1024,2048).to(configuration.device)
+        self.reslin2 = torch.nn.Linear(2048,2048).to(configuration.device)
+        self.reslin3 = torch.nn.Linear(2048,2048).to(configuration.device)
+        self.reslin  = torch.nn.Linear(2048,512).to(configuration.device)
 
-        self.src_upscale  = torch.nn.Linear(49,256).to(configuration.device)
-        self.tgt_upscale  = torch.nn.Linear(2,256).to(configuration.device)
+        self.src_position = torch.nn.Embedding(392,512).to(configuration.device)
 
-        self.downscale = torch.nn.Linear(256,2).to(configuration.device)
-        self.encoder   = torch.nn.TransformerEncoder(torch.nn.TransformerEncoderLayer(256,nhead=4,dim_feedforward=1024,activation="relu",dropout=.1),num_layers=2).to(configuration.device)
-        self.decoder   = torch.nn.TransformerDecoder(torch.nn.TransformerDecoderLayer(256,nhead=4,dim_feedforward=1024,activation="relu",dropout=.1),num_layers=2).to(configuration.device)
-
-        self.peek      = Model.generate_hourglass_subsequent_mask(70).to(configuration.device)
+        self.downscale = torch.nn.Linear(512,2).to(configuration.device)
+        self.encoder   = torch.nn.TransformerEncoder(torch.nn.TransformerEncoderLayer(512,nhead=8,dim_feedforward=2048,activation="gelu",dropout=.1),num_layers=4).to(configuration.device)
 
         self.normalize = torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])
 
-        self.loss         = torch.nn.L1Loss(reduction="sum")
+        self.loss         = torch.nn.L1Loss()
         self.optimizer    = torch.optim.Adam(self.parameters(),lr=0.0001)
-
         self.all_parameters = sum(p.numel() for p in self.parameters())
         self.trainable_parameters = sum(p.numel() for p in self.parameters() if p.requires_grad)
+        self.augment = Augment(self.configuration)
 
     @staticmethod
     def generate_square_subsequent_mask(sz):
@@ -53,22 +57,37 @@ class Model(torch.nn.Module):
             data = data.to(self.configuration.device)
             gold = gold.to(self.configuration.device)
 
-            res = self.resnet(self.normalize(data))
-            res = res.view(res.size(0),res.size(1),res.size(2)*res.size(3))
+            if self.training and self.configuration.do_augmentation: data,gold = self.augment.random_flip(data,gold)    
 
-        srcs = self.src_upscale(res)
+        res0 = self.resnet0(self.normalize(data))
+        res1 = self.resnet1(res0)
+        res2 = self.resnet2(res1)
+        res3 = self.resnet3(res2)
 
-        tgts = torch.cat([self.sos.weight.unsqueeze(0).repeat(gold.size(0),1,1),
-            self.tgt_upscale(gold[:,7:]),
-                          self.eos.weight.unsqueeze(0).repeat(gold.size(0),1,1)],1)
+        #print(res0.shape, res1.shape, res2.shape, res3.shape)
 
-        srcs += self.src_position.weight.unsqueeze(0).repeat(gold.size(0),1,1)
+        # divide intermediate representation in patches + linear
+        res0 = self.reslin(fun.dropout(fun.gelu(self.reslin0(einops.rearrange(res0, "b c (h1 h2) (w1 w2) -> b (h2 w2) (c h1 w1)",h1=2,w1=2))),p=.1))
+        res1 = self.reslin(fun.dropout(fun.gelu(self.reslin1(einops.rearrange(res1, "b c (h1 h2) (w1 w2) -> b (h2 w2) (c h1 w1)",h1=2,w1=2))),p=.1))
+        res2 = self.reslin(fun.dropout(fun.gelu(self.reslin2(einops.rearrange(res2, "b c (h1 h2) (w1 w2) -> b (h2 w2) (c h1 w1)",h1=2,w1=2))),p=.1))
+        res3 = self.reslin(fun.dropout(fun.gelu(self.reslin3(einops.rearrange(res3, "b c h w -> b (h w) c"))),p=.1))
 
-        tgts += self.tgt_position.weight.unsqueeze(0).repeat(gold.size(0),1,1)
+        #print(res0.shape, res1.shape, res2.shape, res3.shape)
 
-        mems = self.encoder(srcs.transpose(0,1))
-        pred = self.decoder(memory=mems, tgt=tgts.transpose(0,1), tgt_mask=self.peek).transpose(0,1)
-        ldmk = self.downscale(pred)
-        ldmk = torch.cat([ldmk[:,:ldmk.size(1)//2-1],ldmk[:,ldmk.size(1)//2+1:]],1)
+        # maxpool on channels
+        res0 = einops.reduce(res0, "b (c1 c2) e -> b c1 e","max",c2=16)
+        res1 = einops.reduce(res1, "b (c1 c2) e -> b c1 e","max",c2=8)
+        res2 = einops.reduce(res2, "b (c1 c2) e -> b c1 e","max",c2=4)
+
+        #print(res0.shape, res1.shape, res2.shape, res3.shape)
+
+        res = torch.cat([res0,res1,res2,res3],1)
+
+        #print(res.shape)
+
+        srcs = res + self.src_position.weight.unsqueeze(0).repeat(gold.size(0),1,1)
+        mems = self.encoder(srcs.transpose(0,1)).transpose(0,1)
+        ldmk = self.downscale(mems[:,:68])
 
         return torch.cat([gold[:,:7],ldmk],1)
+
