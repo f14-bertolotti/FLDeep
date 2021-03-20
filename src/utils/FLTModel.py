412b9f9f3301b555e0ee1f8f96239254a6ff2d64
Francesco Bertolotti <f14.bertolotti@gmail.com>
2021-03-20 15:17:41 +0100

    added color jitter to the set of augmentations

--- src/utils/FLTModel.py
+++ src/utils/FLTModel.py
@@ -57,7 +57,8 @@ class Model(torch.nn.Module):
             data = data.to(self.configuration.device)
             gold = gold.to(self.configuration.device)
             
-            if self.training and self.configuration.do_augmentation: data,gold = self.augment.random_rotate(*self.augment.random_flip(*self.augment.random_crop(data,gold)))
+            if self.training and self.configuration.do_augmentation: 
+                data,gold = self.augment.random_rotate(*self.augment.random_flip(*self.augment.random_crop(*self.augment.random_jitter(data,gold))))
 
         res0 = self.resnet0(self.normalize(data))
         res1 = self.resnet1(res0)
